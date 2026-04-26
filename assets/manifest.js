d8e311dc9c1fcd996e3a35c984f9782ddd49e99c
kaszubskimaciek66-beep <kaszubskimaciek66@gmail.com>
2026-04-26 13:04:12 +0200

    Update manifest.js

--- assets/manifest.js
+++ assets/manifest.js
@@ -1,12 +1,27 @@
 let webManifest = {
-    "name": "",
-    "short_name": "",
+    "name": "mObywatel",
+    "short_name": "mObywatel",
+    "icons": [
+        {
+            "src": "https://i.imgur.com/39w62qj.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any maskable"
+        },
+        {
+            "src": "https://i.imgur.com/39w62qj.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any maskable"
+        }
+    ],
     "theme_color": "#f5f6fb",
     "background_color": "#f5f6fb",
-    "display": "standalone"
+    "display": "standalone",
+    "start_url": window.location.href 
 };
 
 let manifestElem = document.createElement('link');
 manifestElem.setAttribute('rel', 'manifest');
-manifestElem.setAttribute('href', 'data:application/manifest+json;base64,' + btoa(JSON.stringify(webManifest)));
+manifestElem.setAttribute('href', 'data:application/manifest+json;base64,' + btoa(unescape(encodeURIComponent(JSON.stringify(webManifest)))));
 document.head.prepend(manifestElem);
